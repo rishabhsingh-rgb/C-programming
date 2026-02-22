0ad4755f6e955dd7fa125817d7776311cb69b3ce
Rishabh Singh <rishabhsingh742006@gmail.com>
2026-02-22 23:37:03 +0530

    Checks whether a number is palindrome or not
    Implement palindrome number checker in C.

---
+++ palindrome_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int num,rem,rev_num=0,org_num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    org_num=num;
+    while(num>0)
+    {
+        rem=num%10;
+        rev_num=rev_num*10+rem;
+        num=num/10;
+    }
+    if(rev_num==org_num)
+    {
+        printf("%d is a palindrome number.",org_num);
+    }
+    else
+    {
+        printf("%d is not a palindrome number.",org_num);
+    }
+}
